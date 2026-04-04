9543c68dc5268edab303272b5a67e2ae3d41b5a7
Yaroslav <62301460+yaroslav20568@users.noreply.github.com>
2026-04-04 09:08:51 -0400

    feat: init app

---
+++ src/game/main.ts
@@ -0,0 +1,21 @@
+import { Game as MainGame } from './scenes/Game';
+import { AUTO, Game, Types } from 'phaser';
+
+// Find out more information about the Game Config at:
+// https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
+const config: Types.Core.GameConfig = {
+    type: AUTO,
+    width: 1024,
+    height: 768,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scene: [
+        MainGame
+    ]
+};
+
+const StartGame = (parent: string) => {
+    return new Game({ ...config, parent });
+}
+
+export default StartGame;
